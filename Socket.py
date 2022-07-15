ad94ee014d43fae59d5e05db1e3aabec125e1d1e
gov1832 <gov1832@naver.com>
2022-07-15 19:21:41 +0900

    op send

--- Socket.py
+++ Socket.py
@@ -11,29 +11,90 @@ class Socket_function:
         self.client_socket.connect((ip, port))
 
     def socket_send_msg(self, send_msg):
-        self.client_socket.send(send_msg.encode())
+        self.client_socket.send(send_msg.encode('utf-16'))
 
         # recv
-        recv_msg = self.client_socket.recv(1024)
-        d_recv_msg = recv_msg.decode()
-        print("recv: " + recv_msg.decode())
-        for i in range(len(recv_msg)):
-            print("msg[i]: ", recv_msg[i])
-
-        for i in range(len(recv_msg)):
-            print("de_msg[i]: ", recv_msg[i])
+        # recv_msg = self.client_socket.recv(1024)
+        # d_recv_msg = recv_msg.decode()
+        # print("recv: " + recv_msg.decode())
+        # for i in range(len(recv_msg)):
+        #     print("msg[i]: ", recv_msg[i])
+        #
+        # for i in range(len(recv_msg)):
+        #     print("de_msg[i]: ", recv_msg[i])
 
     def socket_read(self):
-        return self.client_socket.recv(1)
+        return self.client_socket.recv(1024)
 
-    def send_FF_msg(self):
-        sender_ip = '123.456.789.123-'
-        destination_ip = '127.000.000.001-'
+    def send_FF_res_msg(self, sender_ip, destination_ip):
         controller_kind = 'VD'
-        controller_number = '00000'
-        length = '0001'
-        opcode = 'FF'
-
-        send_msg = sender_ip + destination_ip + controller_kind + controller_number + length + opcode
+        controller_number = '12345'
+        length = '0002'
+        point = chr(0x2D)
+        opcode = chr(0xFF)
+        data = chr(0x06) # ack
+        # opcode = bytes([0xff])
+        send_msg = sender_ip + point + destination_ip + point + controller_kind + controller_number + length + opcode + data
         print(send_msg)
         self.socket_send_msg(send_msg)
+
+    def send_FF_msg(self):
+        sender_ip = '123.456.789.123'
+        destination_ip = '127.000.000.001'
+        controller_kind = 'VD'
+        controller_number = '12345'
+        length = '0001'
+        point = chr(0x2D)
+        opcode = chr(0xFF)
+
+        send_msg = sender_ip + point + destination_ip + point + controller_kind + controller_number + length + opcode
+        self.socket_send_msg(send_msg)
+
+    def send_FE_msg(self):
+        sender_ip = '123.456.789.123'
+        destination_ip = '127.000.000.001'
+        controller_kind = 'VD'
+        controller_number = '12345'
+        length = '0001'
+        point = chr(0x2D)
+        opcode = chr(0xFE)
+
+        send_msg = sender_ip + point + destination_ip + point + controller_kind + controller_number + length + opcode
+        self.socket_send_msg(send_msg)
+
+    def send_01_msg(self):
+        sender_ip = '123.456.789.123'
+        destination_ip = '127.000.000.001'
+        controller_kind = 'VD'
+        controller_number = '12345'
+        length = '0001'
+        point = chr(0x2D)
+        opcode = chr(0xFE)
+
+        send_msg = sender_ip + point + destination_ip + point + controller_kind + controller_number + length + opcode
+        self.socket_send_msg(send_msg)
+
+    def send_04_msg(self):
+        sender_ip = '123.456.789.123'
+        destination_ip = '127.000.000.001'
+        controller_kind = 'VD'
+        controller_number = '12345'
+        length = '0001'
+        point = chr(0x2D)
+        opcode = chr(0xFE)
+
+        send_msg = sender_ip + point + destination_ip + point + controller_kind + controller_number + length + opcode
+        self.socket_send_msg(send_msg)
+
+    def send_05_msg(self):
+        sender_ip = '123.456.789.123'
+        destination_ip = '127.000.000.001'
+        controller_kind = 'VD'
+        controller_number = '12345'
+        length = '0001'
+        point = chr(0x2D)
+        opcode = chr(0xFE)
+
+        send_msg = sender_ip + point + destination_ip + point + controller_kind + controller_number + length + opcode
+        self.socket_send_msg(send_msg)
+
--- main_function.py
+++ main_function.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 
 import time
 from multiprocessing import Process
+import threading
 
 from db import DB_function
 from Socket import Socket_function
@@ -23,6 +24,9 @@ class main_function(QWidget):
         self.set_ui()
         self.btn_event()
 
+        # value setting
+        self.local_ip = '127.0.0.1'
+
     def time_bar_timeout(self):
         now = time.localtime()
         self.ui.time_bar.setText(str("%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)))
@@ -32,10 +36,28 @@ class main_function(QWidget):
         self.ui.sock_ip_input.setText("127.0.0.1")
         self.ui.sock_port_input.setText("3333")
 
-    def btn_event(self):
-        # test
-        self.ui.test_btn.clicked.connect(self.test_btn_click)
+        self.ui.op_FF_btn.setEnabled(False)
+        self.ui.op_FE_btn.setEnabled(False)
+        self.ui.op_01_btn.setEnabled(False)
+        self.ui.op_04_btn.setEnabled(False)
+        self.ui.op_05_btn.setEnabled(False)
+        self.ui.op_07_btn.setEnabled(False)
+        self.ui.op_0C_btn.setEnabled(False)
+        self.ui.op_0D_btn.setEnabled(False)
+        self.ui.op_0D_btn.setEnabled(False)
+        self.ui.op_0E_btn.setEnabled(False)
+        self.ui.op_0F_btn.setEnabled(False)
+        self.ui.op_11_btn.setEnabled(False)
+        self.ui.op_12_btn.setEnabled(False)
+        self.ui.op_13_btn.setEnabled(False)
+        self.ui.op_15_btn.setEnabled(False)
+        self.ui.op_16_btn.setEnabled(False)
+        self.ui.op_17_btn.setEnabled(False)
+        self.ui.op_18_btn.setEnabled(False)
+        self.ui.op_19_btn.setEnabled(False)
+        self.ui.op_1E_btn.setEnabled(False)
 
+    def btn_event(self):
         self.ui.socket_connect_btn.clicked.connect(self.socket_connect_btn_click)
         self.ui.db_connect_btn.clicked.connect(self.db_connect_btn_click)
 
@@ -59,11 +81,13 @@ class main_function(QWidget):
         self.ui.op_18_btn.clicked.connect(self.op_18_btn_click)
         self.ui.op_19_btn.clicked.connect(self.op_19_btn_click)
         self.ui.op_1E_btn.clicked.connect(self.op_1E_btn_click)
+        # test
+        self.ui.test_btn.clicked.connect(self.test_btn_click)
         # endregion
 
 
     def test_btn_click(self):
-        self.sock.socket_send_msg("testttttt")
+        self.sock.socket_send_msg("/end")
 
     # region btn click function
     def socket_connect_btn_click(self):
@@ -79,17 +103,67 @@ class main_function(QWidget):
             except Exception as e:
                 self.ui.status_bar.setText("err socket connect: ", e)
             self.ui.status_bar.setText("Socket server '" + sock_ip + "', '" + str(sock_port) + "' connect !")
+
+            # region test btn true
+            self.ui.op_FF_btn.setEnabled(True)
+            self.ui.op_FE_btn.setEnabled(True)
+            self.ui.op_01_btn.setEnabled(True)
+            self.ui.op_04_btn.setEnabled(True)
+            self.ui.op_05_btn.setEnabled(True)
+            self.ui.op_07_btn.setEnabled(True)
+            self.ui.op_0C_btn.setEnabled(True)
+            self.ui.op_0D_btn.setEnabled(True)
+            self.ui.op_0D_btn.setEnabled(True)
+            self.ui.op_0E_btn.setEnabled(True)
+            self.ui.op_0F_btn.setEnabled(True)
+            self.ui.op_11_btn.setEnabled(True)
+            self.ui.op_12_btn.setEnabled(True)
+            self.ui.op_13_btn.setEnabled(True)
+            self.ui.op_15_btn.setEnabled(True)
+            self.ui.op_16_btn.setEnabled(True)
+            self.ui.op_17_btn.setEnabled(True)
+            self.ui.op_18_btn.setEnabled(True)
+            self.ui.op_19_btn.setEnabled(True)
+            self.ui.op_1E_btn.setEnabled(True)
+            # endregion
+
             # read 스레드 시작 while
-            # read_socket = Process(target=self.read_socket_msg)
+            t = threading.Thread(target=self.read_socket_msg, args=())
+            t.start()
+            # read_socket = Process(target=self.read_socket_msg, args=())
             # read_socket.start()
 
     def db_connect_btn_click(self):
         print("db..")
         self.db.test()
 
+    # endregion
+
+    # region socket_msg
+
     def read_socket_msg(self):
         while 1:
-            self.sock.socket_read()
+            recv_msg = self.sock.socket_read()
+            if recv_msg == '':
+                break
+            else:
+                self.parsing_msg(recv_msg)
+
+    def parsing_msg(self, recv_msg):
+        print("recv_msg: ", recv_msg)
+        print("d_recv_msg: ", recv_msg.decode('utf-16'))
+        d_recv_msg = recv_msg.decode('utf-16')
+
+        for i in range(len(d_recv_msg)):
+            print(i, "   ", d_recv_msg[i])
+        # print(msg_op)
+        if len(d_recv_msg) > 40:
+            msg_op = d_recv_msg[43]
+            if msg_op == chr(0xFF):
+                sender_ip = d_recv_msg[0:15]
+                self.sock.send_FF_res_msg(self.local_ip, sender_ip)
+
+
     # endregion
 
     # region test send msg
@@ -99,27 +173,35 @@ class main_function(QWidget):
 
     def op_FE_btn_click(self):
         print("btn_click")
+        self.sock.send_FE_msg()
 
     def op_01_btn_click(self):
         print("btn_click")
+        self.sock.send_01_msg()
 
     def op_04_btn_click(self):
         print("btn_click")
+        self.sock.send_04_msg()
 
     def op_05_btn_click(self):
         print("btn_click")
+        self.sock.send_05_msg()
 
     def op_07_btn_click(self):
         print("btn_click")
+        self.sock.send_07_msg()
 
     def op_0C_btn_click(self):
         print("btn_click")
+        self.sock.send_0C_msg()
 
     def op_0D_btn_click(self):
         print("btn_click")
+        self.sock.send_0D_msg()
 
     def op_0E_btn_click(self):
         print("btn_click")
+        self.sock.send_0E_msg()
 
     def op_0F_btn_click(self):
         print("btn_click")
